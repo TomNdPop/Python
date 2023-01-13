2958d4aafd83f8cbdce0850d1ff2ebed27338d96
TomNdPop <lilleymanryan2020@gmail.com>
2023-01-12 19:54:34 -0500

    Add project12.py

---
+++ Project/Chapter2/project12.py
@@ -0,0 +1,16 @@
+#Calculating jeff's various earnings and debts
+#before selling
+amountPaid = 2000*40
+amountPaidtoBroker = amountPaid*0.03
+amountLeftInitial = amountPaid - amountPaidtoBroker
+#after selling
+amountAfter = 2000*42.75
+amountPaidAftertoBroker = amountAfter*.03
+amountLeftFinal = amountPaid - amountPaidAftertoBroker
+#calculating the total profit or loss
+profit = amountLeftFinal - amountLeftInitial
+
+# displaying the various asked quantities
+print()
+
+
