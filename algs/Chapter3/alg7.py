367b76bc2637b911f3ed208477ee31166734ad81
TomNdPop <lilleymanryan2020@gmail.com>
2023-01-17 18:10:42 -0500

    Remove 8,9,10 and add 7

---
+++ algs/Chapter3/alg7.py
@@ -0,0 +1,8 @@
+#Asking for input to points
+points = float(input('Enter a points value: '))
+
+#if structure
+if points>9 and points<51:
+    print('Valid Points!')
+else:
+    print('Invalid Points!')
