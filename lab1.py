8ba101ac02b0164e8731eb25ad3dd9f62c9f7fd5
TomNdPop <lilleymanryan2020@gmail.com>
2023-01-17 14:52:12 -0500

    Add lab dir and first lab1.py

---
+++ lab1.py
@@ -0,0 +1,11 @@
+# Storing name, course name, age
+name = str(input('Enter your name: '))
+c_name = str(input('Enter your course name: '))
+age = float(input('Enter your age: '))
+
+#Calculating age in months
+month = age*12
+#Printing name, cn, and age in months
+print(name)
+print(c_name)
+print(month)
