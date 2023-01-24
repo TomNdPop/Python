0c2a46881f045d9955f422569b6e9e639f7ea06c
TomNdPop <lilleymanryan2020@gmail.com>
2023-01-24 12:46:22 -0500

    Add the new user and the new bronco.py

--- PythonExamples/bronco.py
+++ PythonExamples/bronco.py
@@ -1,18 +1,13 @@
-#Asking for user information
-first = str(input('First Name:'))
-last = str(input('Last Name:'))
-username = str(input('Enter your desired user: '))
+bronco = str(input('Enter your bronco id (enter w):'))
 
-#Asking for password
 password = str(input('Enter your desired password (must use 8 characters):'))
 password_match = str(input('Confirm your password:'))
 
-if len(password) < 8:
+if (bronco == 'W'):
     print('Please try again! You need more than 8 characters for a strong password!')
 else:
     if password == password_match:
         print('These passwords match.')
-        print(first,last,username + "@gmail.com",password, end = " ")
+        print(bronco,password, sep="\n")
     else:
         print('These passwords do not match')
-
