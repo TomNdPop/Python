143868dbee132b7f61775d7a67cc3008496aadbd
TomNdPop <lilleymanryan2020@gmail.com>
2023-01-24 12:19:28 -0500

    Add the project4.py

---
+++ Project/Chapter3/project4.py
@@ -0,0 +1,24 @@
+roman = int(input("Enter a number between 1 - 10:"))
+
+if roman == 1:
+    print('The roman equivalent is I')
+elif roman == 2:
+    print('The roman equivalent is II')
+elif roman == 3: 
+    print('The roman equivalent is III')
+elif roman == 4:
+    print('The roman equivalent is IV')
+elif roman == 5:
+    print('The roman equivalent is V')
+elif roman == 6:
+    print('The roman equivalent is VI')
+elif roman == 7:
+    print('The roman equivalent is VII')
+elif roman == 8:
+    print('The roman equivalent is VIII')
+elif roman == 9: 
+    print('The roman equivalent is IX')
+elif roman == 10:
+    print('The roman equivalent is X')
+else:
+    print('Invalid')
